f1881a8ca983e34eb753646e651421298b4bd0ef
NitrenePL <1swordcreamcold14z@gmail.com>
2024-11-13 15:11:20 +0800

    校准电压值

--- Template/USER/main.c
+++ Template/USER/main.c
@@ -6,21 +6,21 @@
 
 volatile u32 ADC1_Val     = 0;
 volatile u32 ADC1_Val_AVG = 0;
+volatile double ADC1_Volt = 0;
 
 int main(void)
 {
     //  中断采用2:2分组
     NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);
     //  初始化ADC1 及 采样时钟
-    // ADC1_Init();
-    // ADC1_TIM10_Init(1000 - 1, 168 - 1); // 1kHz 采样率 待定
     ADC1_IN6_Init();
-    ADC1_TIM3_Init(1000 - 1, 84 - 1);
+    ADC1_TIM3_Init(100 - 1, 84 - 1);
 
     uart_init(115200);
     delay_init(168);
 
     LCD_Init();
+    LCD_ShowString(30, 80, 400, 16, 16, "STM32F407ZGT6 BUCK @NitrenePL");
 
     while (1) {
     }
@@ -40,9 +40,8 @@ void ADC_IRQHandler(void)
             ADC1_Times++;
         }
         if (ADC1_Times == SAMPLE_TIMES) {
-            // adc2_in7_val_avg = adc2_in7_val / ADC2_SAMPLE_TIMES;
-            // adc2_in7_val     = 0;
             ADC1_Val_AVG = ADC1_Val / SAMPLE_TIMES;
+            ADC1_Volt    = (double)ADC1_Val_AVG * 0.0036 - 0.005;
             ADC1_Val     = 0;
             ADC1_Times   = 0;
         }
